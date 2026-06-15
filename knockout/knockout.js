7c815dbd6f921ef694d2e9dbcdb67d0661e4191e
Ghozi Bimo R <56252960+ghozibimoruci@users.noreply.github.com>
2026-06-15 12:22:31 +0700

    init knockout page

---
+++ knockout/knockout.js
@@ -0,0 +1,39 @@
+// Data Babak Knock Out - 4 Matches, 8 Players
+const knockoutMatches = [
+  {
+    match: 1,
+    player_1: "Vinanda Kristianti",
+    seed_1: "Juara Group A",
+    player_2: "Wildan Viado Elvana Putra",
+    seed_2: "Runner-up Group D",
+    date: "2026-06-16 12:00",
+    winner: null
+  },
+  {
+    match: 2,
+    player_1: "Gestyar Satrio Adji",
+    seed_1: "Juara Group C",
+    player_2: "Asnan Asrawi",
+    seed_2: "Runner-up Group B",
+    date: "2026-06-17 12:00",
+    winner: null
+  },
+  {
+    match: 3,
+    player_1: "Enrico Raynaldo",
+    seed_1: "Juara Group B",
+    player_2: "Juwandy Susilo",
+    seed_2: "Runner-up Group C",
+    date: "2026-06-18 12:00",
+    winner: null
+  },
+  {
+    match: 4,
+    player_1: "Arif Setiadi",
+    seed_1: "Juara Group D",
+    player_2: "Resti",
+    seed_2: "Runner-up Group A",
+    date: "2026-06-19 12:00",
+    winner: null
+  }
+];
